a1df3b7051291234dca1103fbc48cb9fd212fd97
Vicky <vicky@Vikrams-MBP.lan>
2024-06-16 22:16:08 +0530

    Updated padding

--- src/FormPage.js
+++ src/FormPage.js
@@ -3,8 +3,8 @@ import React from "react";
 
 export const FormPage = () => {
     return(
-        <div style={{ paddingTop: 110, padding: 20, backgroundColor: "#ffe9d1", height: '100vh'}}>
-            <h1>
+        <div style={{ padding: 20, backgroundColor: "#ffe9d1", height: '100vh'}}>
+            <h1 style={{paddingTop: 90}}>
             Thank you for your interest! We are currently working on this form to provide you with the best possible experience. It will be available soon. Please check back later.
             </h1>
         </div>
